91ec6113874015f8a646017664b5299e01437359
Shelby Jueden <shelbyjueden@gmail.com>
2014-04-07 22:12:00 -0700

    Eliminated need for mcManager.php for the newest link and version.

--- mcManager.php
+++ mcManager.php
@@ -16,66 +16,7 @@ $curlUserAgent='Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.2.13) Gecko/
 /*
 This grabs the current https://minecraft.net/download page and parses it to get the url of the current vanilla server download.
 */
-if( isset($_GET['getNewVanillaLink']) )
-{
-	// 
-	$url='https://minecraft.net/download';
-
-	//--Dump XML into string
-	$ch = curl_init();
-
-
-	$options = array(
-	    CURLOPT_URL => $url, 
-	    CURLOPT_HEADER => 0, 
-	    CURLOPT_RETURNTRANSFER => 1, 
-	    CURLOPT_FOLLOWLOCATION => TRUE,
-	    CURLOPT_USERAGENT => $curlUserAgent,
-	    CURLOPT_HTTPHEADER => $curlHeader
-	);
-	curl_setopt_array($ch, $options);
-
-	$pageSource = curl_exec($ch);
-	curl_close($ch);
-
-	$linkEnd = strpos($pageSource, '.jar" class="download-link" data-dist="server"');
-	$linkStart = strpos(substr($pageSource, $linkEnd - 120, 120 ),'http') + $linkEnd - 120;
-	//echo 'Start: '.$linkStart.' End: '.$linkEnd.'Length: '.($linkEnd - $linkStart)."\n";
-	echo substr($pageSource,$linkStart, $linkEnd - $linkStart).'.jar';
-
-}
-elseif( isset($_GET['getNewVanillaVersion']) )
-{
-	// 
-	$url='http://minecraft.gamepedia.com/Minecraft';
-
-	//--Dump XML into string
-	$ch = curl_init();
-
-
-	$options = array(
-	    CURLOPT_URL => $url, 
-	    CURLOPT_HEADER => 0, 
-	    CURLOPT_RETURNTRANSFER => 1, 
-	    CURLOPT_FOLLOWLOCATION => TRUE,
-	    CURLOPT_USERAGENT => $curlUserAgent,
-	    CURLOPT_HTTPHEADER => $curlHeader
-	);
-	curl_setopt_array($ch, $options);
-
-	$pageSource = curl_exec($ch);
-	curl_close($ch);
-
-	$area = substr( $pageSource, strpos($pageSource, '<dt>Computer</dt>'));
-	$linkStart = strpos($area, "\">")+2+strpos($pageSource, '<dt>Computer</dt>');
-	$linkEnd = strpos(substr($pageSource, $linkStart ), '</a>')+$linkStart;
-
-	//echo 'Start: '.$linkStart.' End: '.$linkEnd.'Length: '.($linkEnd - $linkStart)."\n";
-	echo substr($pageSource,$linkStart, $linkEnd - $linkStart);
-
-}
-
-elseif( isset($_GET['getCurrentVersion']) )
+if( isset($_GET['getCurrentVersion']) )
 {
 	// Setup Connection
 	$Socket = @FSockOpen( 'udp://'.$serverAddress, 25565, $ErrNo, $ErrStr, 3 );
