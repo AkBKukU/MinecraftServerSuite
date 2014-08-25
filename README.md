8fab60fb220e29ce287cf4fbedcaf2e8774e4c8b
AkBKukU <shelby@jueden.org>
2014-08-25 08:35:22 -0700

    Moved general use functions to external file that bundles them on install.
    forgot to add ecternal file

--- README.md
+++ README.md
@@ -3,19 +3,19 @@ MinecraftServerSuite
 
 Set suite of server tools to make the Minecraft server run like a daemon/service on Ubuntu Linux. This is meant to be an all in one solution for managing the files of a Minecraft server. It will install Minecraft and configure it to run as a service. 
 
-this requires that you have `screen`. If you do not install it with `sudo apt-get install screen`.
+this requires that you have `screen`. If you do not, install it with `sudo apt-get install screen`.
 
 ##Setup##
 
  - You can download the files using the command `git clone https://github.com/AkBKukU/MinecraftServerSuite.git`. 
  - It will create a new directory. Use `cd MinecraftServerSuite` to enter it.
  - Run `sudo mcManager installMinecraft` to start a barebones setup
- - The Minecraft server will start automatically on boot but to run it immediately you will need to run `sudo service minecraft start`. 
+ - The Minecraft server will start automatically on boot but to run it immediately you will need to run `sudo service minecraftd start`. 
 
 That's all there is to the basic setup. 
 
 ##Commands##
-The service will accept the following commands: start, stop, status, restart, console. Run them like this `sudo service minecraft start`
+The service will accept the following commands: start, stop, status, restart, console. Run them like this `sudo service minecraftd start`
 
  - start:
 	 - Runs the server if it's not running.
@@ -29,7 +29,7 @@ The service will accept the following commands: start, stop, status, restart, co
 	 - Brings you to the server console using screen. Exit the console by pressing `ctrl+a+d`
 
 
-The mcManger has several useful commands including: installMinecraft, checkForUpdate, autoUpdate, updateMinecraft, backupMinecraft, autoBackupMinecraft. Run them like this `sudo mcManager checkForUpdate`
+The mcmanger has several useful commands including: installMinecraft, checkForUpdate, autoUpdate, updateMinecraft, backupMinecraft, autoBackupMinecraft. Run them like this `sudo mcmanager checkForUpdate`
 
  - installMinecraft:
 	 - Installs the Minecraft server like described in install instructions.
---
+++ basicFunctions.sh
@@ -0,0 +1,123 @@
+# includeScript
+# 
+# Embeds the specified script where the given comment is first located
+function includeScript ()
+{
+	includeScript="$1"
+	includeLocation="$2"
+	thisScript="./mcmanager"
+	mainCode=$(cat ./mcmanager)
+	includeCode=$(cat $includeScript)
+
+	# Find include location
+	includePoint=$(awk -v a="$mainCode" -v b="$includeLocation" 'BEGIN{print index(a,b)}')  &>> /dev/null
+	afterInclude=$(expr $includePoint + ${#includeLocation})
+
+	before=${mainCode:0:$includePoint}
+
+	after=${mainCode:afterInclude}
+
+	builtCode="$before$includeCode$after"
+	
+	echo "$builtCode"
+}
+
+function includeExample ()
+{
+
+	# Include basicFunctions library during development
+	#    It's kept seperate to make files easier to read
+	if [ -f './basicFunctions.sh' ]
+		then
+
+		source ./basicFunctions.sh
+	fi
+
+	# Insert basicFunctions library during install
+	#[INSERT_FUNCTIONS_HERE]
+
+	includeScript "./basicFunctions.sh" "#[INSERT_FUNCTIONS_HERE]" > "$mcDir/mcmanager"
+}
+
+
+# getYesNo
+# Asks the user the given question and returns a 1 for yes and a 0 for no
+function getYesNo ()
+{	
+	output=2
+	message=$1
+	valid=false
+	until [ $valid == true ]
+	do
+	
+	echo -n "$message (y/n): "
+	read choice
+	choice=$(echo "${choice^^}" )
+		case $choice in
+			"Y"|"YES")
+				output=1
+				valid=true
+				;;
+
+			"N"|"NO")
+				output=0
+				valid=true
+				;;
+		esac
+	done
+
+	return $output
+}
+
+
+# rootTest
+# Tests if the script was run qith root permissions and tells the user they need to if they didn't.
+function rootTest ()
+{
+	if [[ $EUID -ne 0 ]] 
+	then
+   		echo "You need to run this as root to be able to make system changes"
+		echo -e "Run \"sudo $0\" next time"
+		exit 1
+	fi
+}
+
+# commandInfo
+# Says message given the runs command. Once complete adds result to message line.
+function commandInfo ()
+{
+	output=2
+	RED=$(tput setaf 1)
+	GREEN=$(tput setaf 2)
+	NORMAL=$(tput sgr0)
+	PURPLE=$(tput setaf 5)
+
+	message=$1
+	commands=$2
+	echo -n "$message"
+
+    let COL=$(expr $(tput cols) - ${#message})
+	`$commands` &>> /dev/null
+	tempResult=$?
+	if [ $tempResult == 0 ]
+	then
+	    printf "%s%${COL}s" "" "[$GREEN OK  $NORMAL]"
+	    echo
+		output=0
+
+	elif [ $tempResult == 1 ]
+	then
+	    printf "%s%${COL}s" "" "[$RED FAIL $NORMAL]"
+	    echo
+		output=1
+
+	else
+	    printf "%s%${COL}s" "" "[$PURPLE ???? $NORMAL]"
+	    echo
+		output=2
+
+
+	fi
+
+	return $output
+}
